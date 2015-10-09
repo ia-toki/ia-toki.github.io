7aede9461965576a8983db15eda9f5dda566b44c
Jordan Fernando <fernandojordan.92@gmail.com>
2015-10-09 22:31:12 +0700

    Add integration to firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
+	$("#text-success").hide();
+	$("#text-fail").hide();
 	var firebase = new Firebase('https://ngoding.firebaseio.com/email_list');
 	$("#emailForm").submit(function(e) {
 		e.preventDefault();
 		var email = $("#inputEmail").val();
-		firebase.push(email);
-		/*firebase.child(email).once('value', function(snapshot) {
-		    var exists = (snapshot.val() !== null);
-		    if (exists) {
-		    } else {
+		firebase.once('value', function(snapshot) {
+			var exists = false;
+			for (item in snapshot.val()) {
+				if (snapshot.val()[item] === email) {
+					exists = true;
+				}
 			}
-		});*/
+			if (exists) {
+				$("#inputEmail").val("");
+				$("#text-success").hide();
+				$("#text-fail").show();
+			} else {
+				firebase.push(email);
+				$("#inputEmail").val("");
+				$("#text-fail").hide();
+				$("#text-success").show();
+			}
+		});
 	});
 });
